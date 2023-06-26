3cbcb1fff98259c395a00c6550a94c8902802b25
Chandukgit <117837728+Chandukgit@users.noreply.github.com>
2023-06-26 17:30:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import Navbar from'./components/Navbar'
+import Middle from './components/Middle'
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+     <Middle/>
+    </div>
+  );
+}
+
+export default App;
